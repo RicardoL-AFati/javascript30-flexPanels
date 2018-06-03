0d06fef4bc6c48c9c657aa9135e54c42f9874e37
RicardoL-AFati <rickydem24@gmail.com>
2018-06-03 14:55:38 -0600

    added comments

--- script.js
+++ script.js
@@ -5,8 +5,11 @@ function toggleOpen() {
 }
 
 function toggleActive(e) {
+  /* Safari transitionend event.propertyName === flex */ /* Chrome + FF === flex-grow */
+  // multiple transitions ending, if its flex - toggle open-active.
   if (e.propertyName.includes('flex')) this.classList.toggle('open-active');
 }
-
+// Adding click event to each panel to open
 panels.forEach(panel => panel.addEventListener('click', toggleOpen));
+// once each panel is done opening - first and last p elements are shown 
 panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
